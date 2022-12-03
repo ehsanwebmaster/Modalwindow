8d15fc6d377c7514076dd6253d3ba8cce394c9ef
Ehsan Farahmand <89188159+ehsanwebmaster@users.noreply.github.com>
2022-12-03 15:20:25 +0330

    all functions added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overly = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overly.classList.remove('hidden');
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overly.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overly.addEventListener('click', closeModal);
